30b6f788e6de8a5cfd894f51d9ecf3b4cf3a6d73
Fukurokudzu <sergei.fukurokudzu@gmail.com>
2023-01-30 22:50:52 +0500

    Linter fixes and rubocop config created #2

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,15 +1,15 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Project, type: :model do
   describe "some Project model testing" do
     it "Correctly creates turbo stream name to use in views" do
       project = build(:project, user_id: 1)
-      expect(project.stream_name).to eql('projects_1')  
+      expect(project.stream_name).to eql('projects_1')
     end
 
     it "Correctly returns currency" do
-      project = build(:project, salary_per_hour: 3600, salary_per_hour_currency: 'RUB')
-      expect(project.salary.currency.to_s).to eql('RUB')
+      project = build(:project, salary_per_hour: 3600, salary_per_hour_currency: "RUB")
+      expect(project.salary.currency.to_s).to eql("RUB")
     end
 
     it "Fails to save project without title" do
