98d876ca4329e1efba5ad23da08d089577e5bfb3
Fukurokudzu <sergei.fukurokudzu@gmail.com>
2023-01-30 20:05:00 +0500

    Testing some project model functions

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  describe "some Project model testing" do
+    it "Correctly creates turbo stream name to use in views" do
+      project = build(:project, user_id: 1)
+      expect(project.stream_name).to eql('projects_1')  
+    end
+
+    it "Correctly returns currency" do
+      project = build(:project, salary_per_hour: 3600, salary_per_hour_currency: 'RUB')
+      expect(project.salary.currency.to_s).to eql('RUB')
+    end
+
+    it "Fails to save project without title" do
+      expect { create(:project, title: "") }.to raise_error.with_message(/Title can't be blank/)
+    end
+  end
+end
