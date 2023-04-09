d096269b3685879b202eacc2ff578f68b5ad5101
Fukurokudzu <sergei.fukurokudzu@gmail.com>
2023-04-09 19:50:53 +0500

    linter fixes

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,13 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="flash"
 export default class extends Controller {
   connect() {
-    console.log("Hello, Stimulus!", this.element)
+    // console.log("Hello, Stimulus!", this.element)
     setInterval(() => {
-      this.element.classList.add("fadeOut");
-      setInterval(() => {this.element.remove()}, 500)
+      this.element.classList.add('fade-out')
+      setInterval(() => { this.element.remove() }, 500)
     }, 3000)
   }
-  
 }
