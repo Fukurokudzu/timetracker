f7028563fa45692b8b90dabc4f85a5aa8b949d73
Fukurokudzu <sergei.fukurokudzu@gmail.com>
2023-04-10 19:44:31 +0500

    one more eslint fix

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,13 +1,13 @@
 import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="flash"
-/*global bootstrap*/
+/* global bootstrap */
 export default class extends Controller {
   connect () {
     const toastLiveExample = document.getElementById('liveToast')
     const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
     toastBootstrap.show()
-    setInterval( () => {
+    setInterval(() => {
       this.element.classList.add('fade-out')
       setInterval(() => { this.element.remove() }, 500)
     }, 5000)
