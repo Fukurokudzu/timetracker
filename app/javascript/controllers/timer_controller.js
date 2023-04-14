fb950f7640f016138f7efeaf964b9ea0a1e08e04
Fukurokudzu <sergei.fukurokudzu@gmail.com>
2023-04-14 23:31:52 +0500

    timer added, sprint view refactoring

---
+++ app/javascript/controllers/timer_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from '@hotwired/stimulus'
+
+// Connects to data-controller="timer"
+export default class extends Controller {
+  connect () {
+    function formatTime (seconds) {
+      const minutes = Math.floor(seconds / 60)
+      const secondsLeft = seconds % 60
+
+      function padTo2Digits (num) {
+        return num.toString().padStart(2, '0')
+      }
+
+      return padTo2Digits(minutes) + ':' + padTo2Digits(secondsLeft)
+    }
+
+    const text = this.element.innerHTML
+    let seconds = (this.data.get('current-ts') - this.data.get('start-at'))
+
+    setInterval(() => {
+      this.element.innerHTML = text + '+ ' + formatTime(seconds++)
+    }, 1000)
+  }
+}
